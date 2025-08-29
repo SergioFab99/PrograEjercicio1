2a5c096e2e69c3ad35956de67271aa2fc2e6f25d
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-08-29 12:08:47 -0500

    se agregó la clase jugador

---
+++ Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograEjercicio1
+{
+    class Jugador : Personaje
+    {
+        public Jugador(string nombre, int salud) : base(nombre, salud)
+        {
+        }
+
+        public void Atacar(Personaje objetivo)
+        {
+            int daño = 20;
+            objetivo.Salud -= daño;
+            Console.WriteLine($"{Nombre} ataca a {objetivo.Nombre} causando {daño} de daño.");
+        }
+
+        public void UsarObjeto(Objeto objeto)
+        {
+            Console.WriteLine($"{Nombre} usa {objeto.Nombre}");
+        }
+    }
+}
