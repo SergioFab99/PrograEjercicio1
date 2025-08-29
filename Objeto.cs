cb284945f27ead595669b0b1f20bb9973065a293
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-08-29 11:46:18 -0500

    se agregó la clase objeto

---
+++ Objeto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograEjercicio1
+{
+    class Objeto
+    {
+        public string Nombre { get; set; }
+
+        public Objeto(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Objeto encontrado: {Nombre}");
+        }
+    }
+}
