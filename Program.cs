e9433d3004d7c224d0761481115bc1a5b32320e7
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-08-29 12:13:17 -0500

    se mofició la clase program para unificar todo y mejorar la interfaz

--- Program.cs
+++ Program.cs
@@ -10,7 +10,42 @@ namespace PrograEjercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Juego de aventura primer commit");
+            Jugador jugador = new Jugador("Héroe", 100);
+            Enemigo enemigo = new Enemigo("Dragón", 80);
+            Objeto objeto = new Objeto("Espada de Fuego");
+
+            Console.WriteLine("AVENTURA DE TEXTOS");
+            Console.WriteLine("Bienvenido al juego textual");
+            Console.WriteLine();
+
+            jugador.MostrarEstado();
+            enemigo.MostrarEstado();
+            objeto.MostrarInfo();
+
+            Console.WriteLine("\n¿Qué deseas hacer?");
+            Console.WriteLine("1. Atacar al dragón canelonni");
+            Console.WriteLine("2. Usar el objeto");
+            Console.WriteLine("3. Huir como cobarde");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    jugador.Atacar(enemigo);
+                    break;
+                case "2":
+                    jugador.UsarObjeto(objeto);
+                    break;
+                case "3":
+                    Console.WriteLine("Has huido como un vil marica");
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida");
+                    break;
+            }
+
+            Console.WriteLine("\nFin del juego.");
             Console.ReadKey();
         }
     }
