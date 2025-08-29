2c74bce6b99dd165194bec4598b054ad5e9bdcd3
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-08-29 11:36:44 -0500

    se agregó la clase Personaje

---
+++ Clase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograEjercicio1
+{
+    class Personaje
+    {
+        public string Nombre { get; set; }
+        public int Salud { get; set; }
+
+        public Personaje(string nombre, int salud)
+        {
+            Nombre = nombre;
+            Salud = salud;
+        }
+
+        public virtual void MostrarEstado()
+        {
+            Console.WriteLine($"{Nombre} tiene {Salud} puntos de salud.");
+        }
+    }
+}
