536e948f4515e4d3db23f274d1b3ddc3d19c1167
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-08-29 11:42:10 -0500

    se agregó la clase enemigo

---
+++ Enemigo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograEjercicio1
+{
+    class Enemigo : Personaje
+    {
+        public Enemigo(string nombre, int salud) : base(nombre, salud)
+        {
+        }
+
+        public override void MostrarEstado()
+        {
+            Console.WriteLine($"El enemigo {Nombre} tiene {Salud} puntos de salud.");
+        }
+    }
+}
